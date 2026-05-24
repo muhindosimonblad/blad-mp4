8e055ce2e4ca900878be37e54d0b9726b812154e
MUHINDO SIMON <muhindosimonblad@gmail.com>
2026-05-24 20:03:30 +0300

    -m

--- code.js
+++ code.js
@@ -59,7 +59,7 @@ function showlogin(){
   document.getElementById("loginform").classList.remove("hidden");
 }
 
-function login(){
+/*function login(){
   let username = document.getElementById("loginusername").value;
   let password = document.getElementById("loginpassword").value;
 
@@ -70,8 +70,9 @@ function login(){
     window.location.href="home.html"
   }
 }
+  */
 
-function signup(){
+/*function signup(){
   let username = document.getElementById("signupusername").value;
   let password = document.getElementById("signuppassword").value;
   let email = document.getElementById("signupemail").value;
@@ -81,6 +82,84 @@ if(username==="" || password==="" || email===""){
     alert("Account created successfully");
     showlogin();
   }
+
+}*/
+
+function signup(){
+  let username = document.getElementById("signupusername").value.trim();
+  let password = document.getElementById("signuppassword").value;
+  let email = document.getElementById("signupemail").value.trim();
+  
+  
+  //checking for empty spaces
+  if(username==="" || password==="" || email===""){
+    alert("please fill in all fields");
+    return;
+  }
+
+    //user length
+    if(username.length<5){
+      alert("user name must be at least 5 characters");
+      return;
+    }
+
+    //valid email
+    let emailregex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailregex.test(email)){
+      alert("plaese enter valid email address");
+      return;
+    }
+
+    //password strength
+    if(password.length<6){
+      alert("password must be at least 6 characters");
+      return;
+    }
+
+
+    //check user if already exist
+    let users=JSON.parse(localStorage.getItem("users")) || [];
+    if(users[username]){
+      alert("user name already taken");
+      return;
+      }
+
+      //save users
+      user[username]={password:password,email:email};
+      localStorage.setItem("users",JSON.stringify(users));
+      alert("account created successfully");
+      showlogin();
+}
+
+function login(){
+ let username = document.getElementById("loginusername").value.trim();
+  let password = document.getElementById("loginpassword").value;
+ 
+ 
+  //checking for empty spaces
+  if(username==="" || password===""){
+    alert("please fill in all fields");
+    return;
+  }
+
+//check if user exist
+  let users=JSON.parse(localStorage.getItem("users")) || [];
+    if(!users[username]){
+      alert("user not found. please sign up first.");
+      return;
+      }
+
+      //check password match
+      if(users[username].password !== password){
+        alert("incorrect password");
+        return;
+      }
+
+
+      //success
+      localStorage.setItem("currentuser",username);
+      alert("log in successful!");
+      window.location.href="home.html";
 
 }
 
